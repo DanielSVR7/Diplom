ae1e3394334e15dd9366da1d72c31dc6a9b8122d
Даниил Свириденко <sviridenko-02@mail.ru>
2022-02-07 23:50:04 +0400

    Добавьте файлы проекта.

---
+++ project1/Views/Controls/ProductPreview.xaml.cs
@@ -0,0 +1,114 @@
+﻿using project1.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace project1.Views.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPreview.xaml
+    /// </summary>
+    public partial class ProductPreview : UserControl, INotifyPropertyChanged
+    {
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        #endregion
+
+        private string _Title;
+        public string Title 
+        { 
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private string _ImagePath;
+        public string ImagePath 
+        { 
+            get => _ImagePath;
+            set => Set(ref _ImagePath, value);
+        }
+
+        private decimal? _Price;
+        public decimal? Price
+        {
+            get => _Price;
+            set => Set(ref _Price, value);
+        }
+        public ProductPreview(Products product)
+        {
+            DataContext = this;
+            InitializeComponent();
+            if (product != null)
+            {
+                img.Height = 120;
+                Price = product.Price;
+                ManufacturerName.Text = product.Manufacturers.CompanyName;
+                Warranty.Text = product.Warranty.ToString() + " месяцев";
+                ImagePath = @"pack://application:,,,/" + product.Image;
+                if (product.Category == 1)
+                {
+                    Title = product.ScreenSizes.ScreenSizeInInches.ToString() + '"' +
+                         " (" + product.ScreenSizes.ScreenSizeInCentimeters.ToString() + " см) " +
+                         "Телевизор " + product.Manufacturers.CompanyName + ' ' + product.Model + ' ' +
+                         product.Colors.ColorName + " [" + product.ScreenResolutions.ScreenResolutionName + ", " +
+                         product.ScreenResolutions.ScreenResolution + ", " + product.BacklightTypes.BacklightTypeName + ", " +
+                         product.OperatingSystems.OperatingSystemName + ']';
+                    Property1.Text = "Разрешение экрана";
+                    Property2.Text = "Поддержка SmartTV";
+                    Property3.Text = "Поддержка HDR";
+                    Value1.Text = product.ScreenResolutions.ScreenResolutionName + " (" + product.ScreenResolutions.ScreenResolution + ')';
+                    if (product.SmartTVSupport == false || product.SmartTVSupport == null)
+                        Value2.Text = "Нет";
+                    else
+                        Value2.Text = "Есть";
+                    if (product.HDRSupport == false || product.HDRSupport == null)
+                        Value3.Text = "Нет";
+                    else
+                        Value3.Text = "Есть";
+                }
+                if (product.Category == 2)
+                {
+                    img.Height = 180;
+                    Title = "Холодильник " + product.FreezerLocations.FreezerLocationName + ' ' + product.Manufacturers.CompanyName + ' ' +
+                        product.Model + ' ' + product.Colors.ColorName + " [" + (product.RefrigeratorVolume + product.FreezerVolume).ToString() + " л, " +
+                        product.Width + " см x " + product.Height + " см x " + product.Depth + " см]";
+                    Property1.Text = "Компоновка";
+                    Property2.Text = "Инверторный компрессор";
+                    Property3.Text = "Зона свежести";
+                    TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+                    Value1.Text = ti.ToTitleCase(product.FreezerLocations.FreezerLocationName);
+                    if (product.InverterCompressor == false || product.InverterCompressor == null)
+                        Value2.Text = "Нет";
+                    else
+                        Value2.Text = "Есть";
+                    if (product.FreshnessZone == false || product.FreshnessZone == null)
+                        Value3.Text = "Нет";
+                    else
+                        Value3.Text = "Есть";
+                }
+            }
+
+        }
+
+        private void MainBorder_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ project1/Views/Windows/Catalog.xaml.cs
@@ -0,0 +1,163 @@
+﻿using project1.Models;
+using project1.Views.Controls;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace project1.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Catalog.xaml
+    /// </summary>
+    public partial class Catalog : Window, INotifyPropertyChanged
+    {
+        bool IsManufacturersShowed = false;
+        ApplianceStoreEntities db = new ApplianceStoreEntities();
+        Categories _SelectedCategory;
+        List<Products> DisplayedProducts;
+        List<Manufacturers> SelectedManufacturers = new List<Manufacturers>();
+        
+        public Categories SelectedCategory
+        {
+            get => _SelectedCategory;
+            set => Set(ref _SelectedCategory, value);
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+        #endregion
+
+        public Catalog()
+        {
+            InitializeComponent();
+            Refresh();
+            DataContext = this;
+        }
+
+        public void Refresh()
+        {
+            foreach (var c in db.Categories)
+            {
+                Button btn = new Button { Margin = new Thickness(5, 5, 5, 0), Content = c, Tag = c.CategoryID };
+                btn.Click += new RoutedEventHandler(CtgrsClick);
+                CategoriesPanel.Children.Add(btn);
+            }
+            foreach (var m in db.Manufacturers)
+            {
+                var cb = new CheckBox();
+                cb.Content = m;
+                cb.Checked += new RoutedEventHandler(ManufacturersCheckBox_Checked);
+                cb.Unchecked += new RoutedEventHandler(ManufacturersCheckBox_Unchecked);
+                ManufacturersPanel.Children.Add(cb);
+            }
+        }
+        public void ManufacturersCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MainBox.Children.Clear();
+            SelectedManufacturers.Add((Manufacturers)((CheckBox)sender).Content);
+            if (DisplayedProducts != null)
+            {
+                List<Products> RemovingList = new List<Products>();
+                foreach (var p in DisplayedProducts)
+                {
+                    if (SelectedManufacturers.Contains(p.Manufacturers))
+                    {
+                        AddProductToDisplay(p);
+                    }
+                    else
+                        RemovingList.Add(p);
+                }
+                foreach(var p in RemovingList)
+                    DisplayedProducts.Remove(p);
+            }
+        }
+        public void ManufacturersCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SelectedManufacturers.Remove((Manufacturers)((CheckBox)sender).Content);
+            DisplayedProducts.Remove((from d in DisplayedProducts where d.Manufacturers == (Manufacturers)((CheckBox)sender).Content select d).Single());
+            if (DisplayedProducts != null)
+            {
+                MainBox.Children.Clear();
+                foreach (var p in DisplayedProducts)
+                {
+                    if (SelectedManufacturers.Contains(p.Manufacturers))
+                        AddProductToDisplay(p);
+                }
+            }
+            if (SelectedManufacturers.Count == 0)
+            { 
+                DisplayedProducts = (from p in db.Products where p.Category == SelectedCategory.CategoryID select p).ToList();
+                DisplayProducts(DisplayedProducts);
+            }
+        }
+        public void CtgrsClick(object sender, RoutedEventArgs e)
+        {
+            int tag = int.Parse(((Button)sender).Tag.ToString());
+            SelectedCategory = (from c in db.Categories where c.CategoryID == tag select c).Single();
+            DisplayedProducts = (from p in db.Products where p.Category == SelectedCategory.CategoryID select p).ToList();
+            DisplayProducts(DisplayedProducts);
+        }
+        public void DisplayProducts(List<Products> products)
+        {
+            if (products != null)
+            {
+                MainBox.Children.Clear();
+                foreach (var p in products)
+                {
+                    ProductPreview pw = new ProductPreview(p);
+                    MainBox.Children.Add(pw);
+                }
+            }
+        }
+        public void AddProductToDisplay(Products product)
+        {
+            if (product != null)
+            {
+                ProductPreview pw = new ProductPreview(product);
+                MainBox.Children.Add(pw);
+            }
+        }
+        private void ManufacturersShow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (IsManufacturersShowed)
+            {
+                ManufacturersPanel.MaxHeight = 40;
+                ManufacturersShow.Text = "Показать ещё..";
+                IsManufacturersShowed = false;
+            }
+            else
+            {
+                ManufacturersPanel.MaxHeight = 1000;
+                ManufacturersShow.Text = "Свернуть..";
+                IsManufacturersShowed = true;
+            }
+        }
+
+        private void b1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (CheckBox cb in ManufacturersPanel.Children)
+            {
+                if (cb.IsChecked == true)
+                {
+
+                }
+            }
+        }
+    }
+}
